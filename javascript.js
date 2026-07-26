43c4f6b95afd9294933f9e36707fe5f7f241824c
JT122121 <joemarie.tayactac1@gmail.com>
2026-07-26 21:13:27 +0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,94 +1,57 @@
 const codeReader = new ZXing.BrowserMultiFormatReader();
 
-
 const button = document.getElementById("startCamera");
 const result = document.getElementById("result");
 
 
-
 button.onclick = async function () {
 
-
-    result.innerHTML = "Opening camera...";
-
+    result.innerHTML = "Scanning...";
 
     try {
 
-
         const devices = await codeReader.listVideoInputDevices();
-
 
         let cameraId = devices[0].deviceId;
 
 
-        // Select rear camera
-
-        devices.forEach(device => {
+        // choose back camera
+        for (let device of devices) {
 
             if (
                 device.label.toLowerCase().includes("back") ||
-                device.label.toLowerCase().includes("rear") ||
-                device.label.toLowerCase().includes("environment")
+                device.label.toLowerCase().includes("rear")
             ) {
-
                 cameraId = device.deviceId;
-
             }
 
-        });
-
+        }
 
 
         codeReader.decodeFromVideoDevice(
-
             cameraId,
-
             "video",
-
-
             (barcode, error) => {
-
 
                 if (barcode) {
 
-
                     result.innerHTML =
-                    "✅ Barcode Found:<br>" +
-                    barcode.text;
-
+                    "✅ Barcode:<br>" + barcode.text;
 
                     console.log(barcode.text);
 
-
-
-                    // stop after scan
-
                     codeReader.reset();
-
 
                 }
 
-
             }
-
-
         );
 
 
+    } catch (error) {
+
+        result.innerHTML = "Camera Error: " + error;
 
     }
-
-    catch(error) {
-
-
-        result.innerHTML =
-        "Camera Error: " + error;
-
-
-        console.log(error);
-
-
-    }
-
 
 };
